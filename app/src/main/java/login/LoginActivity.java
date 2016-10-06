e5e18a2453490a909e0e043fb0837768c04093fa
mc <mc@mcs-iMac.local>
2016-10-06 13:44:38 +0700

    Initial commit

---
+++ app/src/main/java/login/LoginActivity.java
@@ -0,0 +1,19 @@
+package login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mike.loginscreen.R;
+
+/**
+ * Created by mc on 10/6/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
